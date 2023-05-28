cea94844e0ebcf5f87c00f3d35a224e4a567351c
Vincenzo Fehring <vincenzo.fehring@me.com>
2023-05-28 00:37:11 -0500

    Setup some basic communication with Stripe. Updated the implementation todo list.

--- README.md
+++ README.md
@@ -3,3 +3,136 @@
 ## Note: This SDK is not complete or production ready. Please do not use at this package yet.
 
 A Stripe API wrapper and SDK for the Nim programming language.
+
+## Endpoints to Implement
+
+#### Core Resources
+- [ ] Balances
+- [ ] Balance Transactions
+- [ ] Charges
+- [ ] Customers
+- [ ] Disputes
+- [ ] Events
+- [ ] Files
+- [ ] File Links
+- [ ] Mandates
+- [ ] Payment Intents
+- [ ] Setup Intents
+- [ ] SetupAttempts
+- [ ] Payouts
+- [ ] Refunds
+- [ ] Tokens
+
+#### Payment Methods
+- [ ] PaymentMethods
+- [ ] Bank Accounts
+- [ ] Cash Balance
+- [ ] Cards
+- [ ] Sources
+
+#### Products
+- [ ] Products
+- [ ] Prices
+- [ ] Coupons
+- [ ] Promotion Codes
+- [ ] Discounts
+- [ ] Tax Codes
+- [ ] Tax Rates
+- [ ] Shipping Rates
+
+#### Checkout
+- [ ] Checkout Sessions
+
+#### Payment Links
+- [ ] Paymentt Links
+
+#### Billing
+- [ ] Credit Notes
+- [ ] Customer Balance Transactions
+- [ ] Customer Portal
+- [ ] Customer Tax IDs
+- [ ] Invoices
+- [ ] Invoice Items
+- [ ] Plans
+- [ ] Quotes
+- [ ] Subscriptions
+- [ ] Subscription Items
+- [ ] Subscription Schedules
+- [ ] Test Clocks
+- [ ] Usage Records
+
+#### Connect
+- [ ] Accounts
+- [ ] Account Links
+- [ ] Account Sessions
+- [ ] Application Fees
+- [ ] Application Fee Refunds
+- [ ] Capabilities
+- [ ] Country Specs
+- [ ] External Accounts
+- [ ] Persons
+- [ ] Top-ups
+- [ ] Transfers
+- [ ] Transfer Reversals
+- [ ] Secret Management
+
+#### Fraud
+- [ ] Early Fraud Warnings
+- [ ] Reviews
+- [ ] Value Lists
+- [ ] Value List Items
+
+#### Issuing
+- [ ] Authorizations
+- [ ] Cardholders
+- [ ] Cards
+- [ ] Disputes
+- [ ] Funding Instructions
+- [ ] Transactions
+
+#### Terminal
+- [ ] Connection Tokens
+- [ ] Locations
+- [ ] Readers
+- [ ] Hardware Orders
+- [ ] Hardware Products
+- [ ] Hardware SKUs
+- [ ] Hardware Shipping Methods
+- [ ] Configurations
+
+#### Treasury
+- [ ] Financial Accounts
+- [ ] Financial Account Features
+- [ ] Transactions
+- [ ] Transaction Entries
+- [ ] Outbound Transfers
+- [ ] Outbound Payments
+- [ ] Inbound Transfers
+- [ ] Receive Credits
+- [ ] Receive Debits
+- [ ] Credit Reversals
+- [ ] Debit Reversals
+
+#### Sigma
+- [ ] Scheduled Queries
+
+#### Reporting
+- [ ] Report Runs
+- [ ] Report Types
+
+#### Financial Connections
+- [ ] Accounts
+- [ ] Account Ownership
+- [ ] Sessions
+- [ ] Transactions
+
+#### Tax
+- [ ] Tax Calculations
+- [ ] Tax Transactions
+
+#### Identity
+- [ ] Verification Sessions
+- [ ] Verification Reports
+
+#### Webhooks
+- [ ] Webhook Endpoints
--- src/StripeKit.nim
+++ src/StripeKit.nim
@@ -1,7 +1,76 @@
-# This is just an example to get you started. A typical library package
-# exports the main API in this file. Note that you cannot rename this file
-# but you can remove it if you wish.
+import strutils
+import strformat
+import httpclient
+import asyncdispatch
+import base64
+import options
+import tables; export tables
+import json
+import uri
 
-proc add*(x, y: int): int =
-  ## Adds two files together.
-  return x + y
+type
+  StripeClient* = ref object of RootObj
+    secret: string
+  
+const BASE_URL = "https://api.stripe.com/v1"
+
+var global_stripe_client = none[StripeClient]()
+
+proc newStripeClient*(secret: string): StripeClient =
+  doAssert secret.startsWith("sk_")
+  new(result)
+  result.secret = secret
+
+proc setStripeClient*(client: StripeClient) =
+  global_stripe_client = some(client)
+
+proc stripeClient*(): StripeClient =
+  if global_stripe_client.isNone:
+    raise newException(ValueError, "No stripe client set")
+  result = global_stripe_client.get()
+
+proc headers(client: StripeClient): HttpHeaders =
+  let auth_value = (&"{client.secret}:").encode()
+  newHttpHeaders({
+    "Authorization": &"basic {auth_value}",
+    "Content-Type": "application/x-www-form-urlencoded",
+  })
+
+proc urlencode(data: Table[string, string]): string {.inline.} =
+  ## Convert to format suitable for an HTTP
+  ## application/x-www-form-urlencoded body
+  for k,v in data.pairs:
+    result.add &"{k.encodeUrl()}={v.encodeUrl()}&"
+  if result.len > 0:
+    result.setLen(result.len - 1)
+
+proc get*(client: StripeClient, path: string, params = initTable[string, string]()): Future[JsonNode] {.async.} =
+  var hclient = newAsyncHttpClient()
+  defer: hclient.close()
+  hclient.headers = client.headers
+  var url = BASE_URL & path
+  if params.len > 0:
+    url = url & "?" & params.urlencode()
+  let req = await hclient.get(url)
+  let body = await req.body
+  result = body.parseJson()
+
+proc post*(client: StripeClient, path: string, data = initTable[string, string]()): Future[JsonNode] {.async.} =
+  var hclient = newAsyncHttpClient()
+  defer: hclient.close()
+  hclient.headers = client.headers
+  var url = BASE_URL & path
+  let req = await hclient.post(url, body = data.urlencode())
+  let body = await req.body
+  result = body.parseJson()
+
+proc delete*(client: StripeClient, path: string, params = initTable[string, string]()): Future[JsonNode] {.async.} =
+  var hclient = newAsyncHttpClient()
+  defer: hclient.close()
+  hclient.headers = client.headers
+  var url = BASE_URL & path
+  if params.len > 0:
+    url = url & "?" & params.urlencode()
+  let req = await hclient.delete(url)
+  let body = await req.body
+  result = body.parseJson()
